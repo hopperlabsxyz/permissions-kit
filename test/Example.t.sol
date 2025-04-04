1c97c3ad360c83e2e21567bf677f1e95ff95dfe6
coviccinelle <coviccinelle@gmail.com>
2025-04-04 09:50:42 +0200

    setup forge test

--- test/Example.t.sol
+++ test/Example.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test} from "../lib/forge-std/src/Test.sol";
 
 contract ExampleTest is Test {
     function setUp() public {
