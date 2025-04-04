7d42e5df951d660b50f44f84f9a4e4aa97a6b899
Julien FRANCAIS <jfrancai@student.42.fr>
2025-04-05 01:50:46 +0200

    deploy role modifier

--- test/Base.t.sol
+++ test/Base.t.sol
@@ -1,34 +1,63 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity >= 0.8.28;
+pragma solidity >=0.8.28;
 
 import "@forge-std/Test.sol";
+import "@forge-std/console.sol";
+import {VmSafe} from "@forge-std/Vm.sol";
 import {TestAvatar} from "@test/TestAvatar.sol";
-import {Roles} from "@rolesV2/Roles.sol";
 
 address constant MODULE_PROXY_FACTORY = 0x000000000000aDdB49795b0f9bA5BC298cDda236;
+address constant ROLE_MASTER_COPY = 0x9646fDAD06d3e24444381f44362a3B0eB343D337;
+
 interface IModuleProxyFactory {
-    function deployModule(address, bytes, uint256) external returns (address);
+    function deployModule(
+        address masterCopy,
+        bytes calldata initializer,
+        uint256 salt
+    ) external returns (address);
+}
+
+interface IRoles {
+    function setUp(bytes memory initParams) external;
+
+    function avatar() external returns (address);
+}
+
+function deployRolesModifier(
+    IModuleProxyFactory factory,
+    address avatar,
+    address owner
+) returns (IRoles) {
+    bytes memory initParams = abi.encodeWithSelector(
+        IRoles.setUp.selector,
+        abi.encode(owner, address(avatar), address(avatar))
+    );
+    IRoles role = IRoles(factory.deployModule(ROLE_MASTER_COPY, initParams, 0));
+    return role;
+}
+
+function deployTestAvatar() returns (TestAvatar) {
+    return new TestAvatar();
 }
 
 contract BaseTest is Test {
     TestAvatar public avatar;
-    Roles public role;
 
-    constructor(){
-        deployAvatar();
-        deployRolesModifier();
-        
-    }
+    IRoles public role;
 
-    function deployAvatar() internal {
-        //instantiate the avatar
-        avatar = new TestAvatar();
-        //check the balance of the avatar
-        assertEq(address(avatar).balance, 0);
+    IModuleProxyFactory public roleFactory =
+        IModuleProxyFactory(MODULE_PROXY_FACTORY);
 
+    VmSafe.Wallet public roleOwner = vm.createWallet("roleOwner");
+    VmSafe.Wallet public roleMember = vm.createWallet("roleMember");
 
-
-    // function deployRolesModifier() internal {
-    //     role = IModuleProxyFactory(MODULE_PROXY_FACTORY).deployModule(address(avatar), abi.encode(address(avatar)), 0);
+    constructor() {
+        avatar = deployTestAvatar();
+        role = deployRolesModifier(
+            roleFactory,
+            address(avatar),
+            roleOwner.addr
+        );
+        assertEq(role.avatar(), address(avatar), "role setup failed");
     }
 }
