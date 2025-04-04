a227f4c3a298c72d578b750dd0e52b7cb9458634
coviccinelle <coviccinelle@gmail.com>
2025-04-04 14:04:23 +0200

    reset remapping, it worked ok

--- test/Base.t.sol
+++ test/Base.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity >=0.8.28;
+pragma solidity 0.8.28;
 
-import "../lib/forge-std/src/Test.sol";
+import "@forge-std/Test.sol";
 
 contract BaseTest is Test {
     constructor() {}
