fb40d86bad8881d83ac38d5696924f35d2160b68
Julien FRANCAIS <jfrancai@student.42.fr>
2025-04-16 09:53:07 +0200

    resupply approve

--- src/protocols/index.ts
+++ src/protocols/index.ts
@@ -1,5 +1,7 @@
 import * as lagoon from "./lagoon"
+import * as resupply from "./resupply"
 
 export const eth = {
-  lagoon: lagoon.eth
+  lagoon: lagoon.eth,
+  resupply: resupply.eth
 }
---
+++ src/protocols/resupply/index.ts
@@ -0,0 +1,57 @@
+import { ChainId } from "zodiac-roles-sdk/."
+import { allowErc20Approve } from "../../conditions"
+import { Address } from "@gnosis-guild/eth-sdk"
+
+
+const ethPools = [
+  {
+    name: "crvUSD_WETH",
+    address: "0xCF1deb0570c2f7dEe8C07A7e5FA2bd4b2B96520D",
+    depositAsset: "0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E"
+  }
+] as const;
+
+export type EthPool = typeof ethPools[number]
+
+export type Target = EthPool['address'] | {
+  address: EthPool['address'],
+  depositAsset?: Address
+}
+
+export type Targets = Target[]
+
+export interface TargetInfo {
+  name: string;
+  address: Address;
+  depositAsset: Address;
+}
+
+function depositCrvUSD(_: ChainId, targetInfo: TargetInfo) {
+  return [
+    ...allowErc20Approve([targetInfo.depositAsset], [targetInfo.address]),
+  ]
+}
+
+function getTargetInfo(target: Target): TargetInfo {
+  if (typeof target === 'string') {
+    const res = ethPools.find((t) => t.address === target);
+    if (res === undefined) {
+      throw new Error("Unknown target");
+    }
+    return res;
+  }
+  if (target.depositAsset === undefined) {
+    throw new Error("Missing depositAsset");
+  }
+  return {
+    name: 'Unknown',
+    address: target.address,
+    depositAsset: target.depositAsset
+  }
+}
+
+export const eth = {
+  manageVault: async ({ targets }: { targets: Targets }) => {
+    return targets.flatMap((target) => depositCrvUSD(1, getTargetInfo(target)))
+  },
+}
