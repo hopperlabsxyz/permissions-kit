cbfa0c548ca5574754cfcb37b27eb341d665519b
Julien FRANCAIS <jfrancai@student.42.fr>
2025-05-15 14:16:30 +0200

    adding etherfi supports

--- eth-sdk/config.ts
+++ eth-sdk/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@gnosis-guild/eth-sdk';
 
 export const contracts = {
   mainnet: {
+    weth: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
     dai: '0x6b175474e89094c44da98b954eedeac495271d0f',
     usdc: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
     lagoon: {
