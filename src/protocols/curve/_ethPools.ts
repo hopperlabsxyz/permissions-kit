5e82cfd06a91d5de1d21689b2aa017d47eb0061e
Julien FRANCAIS <jfrancai@student.42.fr>
2025-05-20 16:42:22 +0200

    adding taceth/eth curve pool

--- src/protocols/curve/_ethPools.ts
+++ src/protocols/curve/_ethPools.ts
@@ -3,5 +3,10 @@ export default [
     address: "0xc522a6606bba746d7960404f22a3db936b6f4f50",
     token0: "0x57aB1E0003F623289CD798B1824Be09a793e4Bec", // reUSD
     token1: "0x0655977FEb2f289A4aB78af67BAB0d17aAb84367" // scrvUSD
+  },
+  {
+    address: "0xc73B0328Bd40Ea35Aad34d0fDC1dBE64C4f9c59F",
+    token0: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // weth
+    token1: "0x294eecec65A0142e84AEdfD8eB2FBEA8c9a9fbad" // tacEth
   }
 ] as const;
