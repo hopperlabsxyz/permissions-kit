d321ee475303e5292223f635d135afbef85f95fd
Julien FRANCAIS <jfrancai@student.42.fr>
2025-05-21 09:14:30 +0200

    fix target address curve

--- test/scripts/applyUpdates.ts
+++ test/scripts/applyUpdates.ts
@@ -129,6 +129,7 @@ const permissions = {
       targets: ["0xc522a6606bba746d7960404f22a3db936b6f4f50", "0xc73B0328Bd40Ea35Aad34d0fDC1dBE64C4f9c59F"],
     }),
   },
+
   convex: {
     deposit: await kit.convex.deposit({ targets: [440] }),
   },
